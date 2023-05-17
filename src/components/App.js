01bc738c35be2fc40e615d55cd7607731836284f
Sidheshwar Choure <sidheshwarchoure@gmail.com>
2023-05-17 12:29:42 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 
-import React from "react";
+//import React from "react";
+import React, { useState } from 'react';
+
 import './../styles/App.css';
 
 const App = () => {
+
+    const [name, setName] = useState('');
+    
+    const handleNameChange = (event) => {
+      setName(event.target.value);
+    }
   return (
     <div>
         {/* Do not remove the main div */}
+        <label htmlFor="nameInput">Enter your name: </label><br></br><br></br>
+      <input
+        id="nameInput"
+        type="text"
+        value={name}
+        onChange={handleNameChange}
+      />
+      {name && <p>Hello {name}!</p>}
     </div>
   )
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+//import React, { useState } from 'react';
+
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
